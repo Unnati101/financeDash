0461e7d4c45585326a9c650e4e90b4d6daf4e1d6
RitvikSh <praveensiarma@gmail.com>
2024-05-04 22:33:03 +0530

    updated file version

--- server/index.js
+++ server/index.js
@@ -12,21 +12,21 @@ dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /*MONGOOSE SETUP*/
 
-const PORT =process.env.PORT || 9000;
-const conn=await mongoose
+const PORT = process.env.PORT || 9000;
+const conn = await mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-   .then(async () => {
-    app.listen(PORT,() => console.log(`Server Port:  ${PORT}`))
-   })
-   .catch((error)=> console.log(`${error} did not connect`));
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port:  ${PORT}`))
+  })
+  .catch((error) => console.log(`${error} did not connect`));
