0113e2e657002dd897693dbd9942bc126477c2bb
Unnati101 <143032635+Unnati101@users.noreply.github.com>
2024-05-05 15:23:08 +0530

    database

--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js"
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
 /*configurations*/
+
 
 dotenv.config()
 const app = express();
@@ -18,6 +22,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/*ROUTES*/
+
+app.use("/kpi",kpiRoutes);
+
 /*MONGOOSE SETUP*/
 
 const PORT = process.env.PORT || 9000;
@@ -28,5 +36,9 @@ const conn = await mongoose
   })
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port:  ${PORT}`))
+    
+    await mongoose.connection.db.dropDatabase();
+    KPI.insertMany(kpis);
+  
   })
   .catch((error) => console.log(`${error} did not connect`));
